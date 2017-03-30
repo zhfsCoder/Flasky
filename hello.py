472e041e8582cb07035ba3390d22ac24488cc6dc
zhfscoder <zhfscoder@hotmial.com>
2017-03-30 11:50:12 +0800

    before bootstrap

--- hello.py
+++ hello.py
@@ -1,12 +1,27 @@
 #-*- coding:utf-8 -*-
 
-from flask import Flask
+from flask import Flask, request, render_template
 
 app=Flask(__name__)
 
+mydict = {'key':123, 'hha':'231'}
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
+@app.route('/reuser/<name>')
+def reuser(name):
+    return render_template('user.html', name=name)
+
+@app.route('/user_agent')
+def agent():
+    user_agent = request.headers.get('User_Agent')
+    return '<h1>Hello world</h1> </br> <p>Your browser is %s</p>' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
